3b060d7ade0bf920d3a4d325a37febd0bc8cafe4
Divlab-softwares <divlabsoftware@gmail.com>
2025-08-30 17:15:30 +0100

    Update Hero.tsx

--- src/app/Components/Hero.tsx
+++ src/app/Components/Hero.tsx
@@ -68,7 +68,7 @@ const Hero = () => {
                             </span>
                         </h1>
                         <p className="my-2 text-md md:text-lg text-center md:text-left">
-                            Nous fesons l'impossible avec maitrise
+                            Nous fesons l'impossible avec maîtrise
                         </p>
                         <a href="whatsapp://send?phone=237652509674" target="_blank"className="btn  shadow-[5-5-0-[0,0,0,0.6]] shadow-3xl hover:shadow-lg bg-gradient-to-br from-white/50 via-white to-white/60 border-none transition-transform duration-300 hover:-translate-y-1 hover:scale-105 text-black rounded-4xl w-50 md:w-fit">
                             <img src={Whatsapp.src} alt="" className="w-6 h-6 rounded-full mr-2" />Discuter sur Whatsapp
