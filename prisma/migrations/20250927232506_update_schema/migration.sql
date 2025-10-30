ffbd310a961fb95a59ee82116bc73238ccbfdbe9
Divlab-softwares <divlabsoftware@gmail.com>
2025-10-30 12:57:06 +0100

    Sauvegarde temporaire avant récupération

--- prisma/migrations/20250927232506_update_schema/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[email]` on the table `Signup` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Signup_email_key" ON "Signup"("email");
